8c79adfd1e548d095fb78355c9ec4b42f81b6700
Simon Marchi <simark@nova.polymtl.ca>
2012-08-12 01:28:13 -0400

    Init

---
+++ ncixhdmon.py
@@ -0,0 +1,60 @@
+from BeautifulSoup import BeautifulSoup
+import urllib
+import re
+from operator import itemgetter
+import datetime
+
+f = urllib.urlopen("http://www.ncix.com/products/?minorcatid=109&po=0&ps=2")
+data = f.read()
+
+soup = BeautifulSoup(data)
+span = soup.findAll(name = "span", attrs = {"class" : "listing"})
+
+products = {}
+
+widestPrice = 0
+widestCap = 0
+
+for s in span:
+	link = s.find('a').string
+	price = s.parent.parent.find('strong').string.strip().strip('$').replace(',','')
+	if len(price) > 0:
+		price = float(price)
+	else:
+		continue
+
+	products[link] = price
+
+
+for p in products:
+	matchTB = re.search("([0-9.]+)\s?TB", p)
+	matchGB = re.search("([0-9.]+)\s?GB", p)
+	if matchTB:
+		cap = float(matchTB.group(1)) * 1000
+	elif matchGB:
+		cap = float(matchGB.group(1))
+	else:
+		print "Warning: capacity not found for " + p
+		cap = -1
+
+	price = products[p]
+	ratio = price / cap
+	products[p] = (price, cap, ratio)
+
+now = datetime.datetime.now()
+print "Generated on " + str(now)
+print "Prix\tTaille\t$/GB\tNom"
+print "---------------------------"
+
+products = [p for p in products.items() if p[1][2] > 0]
+
+def sortkey(element):
+	return element[1][2]
+
+for i in sorted(products, key=sortkey):
+	if i[1][1] > 0:
+		if i[1][1] >= 1000:
+			cap = "%.1f TB" % (i[1][1] / 1000)
+		else:
+			cap = "%d GB" % i[1][1]
+		print "%.2f\t%s\t%.3f\t" % (i[1][0], cap, i[1][2]) + i[0]
